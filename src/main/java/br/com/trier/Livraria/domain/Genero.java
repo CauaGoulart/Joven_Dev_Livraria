39412e0d33c1cc013f8de2cc48b6f4c7fd7b2560
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-06-28 11:45:35 -0300

    Delete src directory

--- src/main/java/br/com/trier/Livraria/domain/Genero.java
+++
@@ -1,5 +0,0 @@
-package br.com.trier.Livraria.domain;
-
-public class Genero {
-
-}
--- src/main/java/br/com/trier/Livraria/services/exceptions/IntegrityViolation.java
+++
@@ -1,9 +0,0 @@
-package br.com.trier.Livraria.services.exceptions;
-
-@SuppressWarnings("serial")
-public class IntegrityViolation extends RuntimeException{
-
-	public IntegrityViolation(String mensagem) {
-		super(mensagem);
-	}
-}
