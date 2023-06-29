810842bde5d4cec8fdec968c8fb68603f4009e48
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-06-29 11:55:13 -0300

    Update

--- target/test-classes/resources/sqls/bookAuthor.sql
+++ target/test-classes/resources/sqls/bookAuthor.sql
@@ -8,8 +8,8 @@ VALUES (1, 'Book 1', 20);
 INSERT INTO book (id_book, title_book, price_book)
 VALUES (2, 'Book 2', 40);
 
-INSERT INTO book_author (id_book-author,author_id_author,book_id_book) 
+INSERT INTO book_author (id_book_author,author_id_author,book_id_book) 
 VALUES (1,1,1);
 
-INSERT INTO book_author (id_book-author,author_id_author,book_id_book) 
+INSERT INTO book_author (id_book_author,author_id_author,book_id_book) 
 VALUES (2,2,2);
---
+++ target/test-classes/resources/sqls/phone.sql
@@ -0,0 +1,9 @@
+INSERT INTO client (id_client, name_client, email_client, password_client)
+VALUES (5, 'test 1', 'test1@gmail.com', '123');
+Insert into phone (id_phone,phone_number,client_id_client)
+VALUES (5,'99999-9999',5);
+
+INSERT INTO client (id_client, name_client, email_client, password_client)
+VALUES (6, 'test 2', 'test2@gmail.com', '123');
+Insert into phone (id_phone,phone_number,client_id_client)
+VALUES (6,'88888-8888',6);
