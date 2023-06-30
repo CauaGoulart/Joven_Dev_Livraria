8dd4bef5a059ffa872fa35ebfda1130beb9bec4e
Cauã <114314281+CauaGoulart@users.noreply.github.com>
2023-06-30 11:26:26 -0300

    Update

--- target/test-classes/resources/sqls/client.sql
+++ target/test-classes/resources/sqls/client.sql
@@ -1,4 +1,4 @@
-INSERT INTO client (id_client, name_client, email_client, password_client)
-VALUES (1, 'test 1', 'test1@gmail.com', '123');
-INSERT INTO client (id_client, name_client, email_client, password_client)
-VALUES (2, 'test 2', 'test2@gmail.com', '123');
+INSERT INTO client (id_client, name_client, email_client, password_client,roles_client)
+VALUES (1, 'test 1', 'test1@gmail.com', '123','USER,ADMIN');
+INSERT INTO client (id_client, name_client, email_client, password_client,roles_client)
+VALUES (2, 'test 2', 'test2@gmail.com', '123','USER,ADMIN');
